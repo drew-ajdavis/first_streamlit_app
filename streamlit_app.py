2855a144340b4ea893521f11afac0dd8a950f076
drew-ajdavis <98888731+drew-ajdavis@users.noreply.github.com>
2022-04-15 16:09:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text(🍜'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐓'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞'Avacado Toast')
+streamlit.text('🍜 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐓 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+streamli.text('🍌🍓 Build Your Owm Smoothie 🥝🫐')
